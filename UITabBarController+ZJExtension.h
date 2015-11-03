ebb1dd55eacb9f6065d8c75eba0db097066adced
leiyu <642459351@qq.com>
2015-11-03 17:45:53 +0800

    first

---
+++ UITabBarController+ZJExtension.h
@@ -0,0 +1,16 @@
+
+//  Created by mac on 15/5/12.
+//  Copyright (c) 2015年 leiyu. All rights reserved.
+//
+
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UITabBarController (ZJExtension)
+//为标签栏控制器添加一个界面
+-(id)addViewControlerWithClass:(Class )cls
+                        title:(NSString *)title
+                         image:(NSString *)image
+                   selectImage:(NSString *)selectImage;
+@end
